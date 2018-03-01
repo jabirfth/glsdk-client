79cb5610f6e60cb4c54e362c69e168ab296216a6
Brice Argenson <bargenson@gmail.com>
2018-03-01 16:22:56 -0500

    Add e2e test

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,27 @@
 import { WebappPage } from './app.po';
 
-describe('webapp App', () => {
+describe('Webapp App', () => {
+
   let page: WebappPage;
 
   beforeEach(() => {
     page = new WebappPage();
   });
 
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+  it('should go to login form and submit it', (done) => {
+    page.navigateTo().then(() => {
+      return page.clickOnLoginButton();
+    }).then(() => {
+      expect<any>(page.getFormTitle()).toEqual('Which provider do you want to use?');
+      return page.enterUsername('admin');
+    }).then(() => {
+      return page.enterPassword('admin');
+    }).then(() => {
+      return page.submitForm();
+    }).then(() => {
+      expect(page.getErrorMessage()).toContain('Unexpected error');
+      done();
+    });
   });
+
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,11 +1,33 @@
 import { browser, element, by } from 'protractor';
 
 export class WebappPage {
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  clickOnLoginButton() {
+    return element(by.css('.icon-button button[title=Login]')).click();
   }
+
+  getFormTitle() {
+    return element(by.css('app-login mat-card-title')).getText();
+  }
+
+  enterUsername(username: string) {
+    return element(by.css('input[name=username]')).sendKeys(username);
+  }
+
+  enterPassword(password: string) {
+    return element(by.css('input[name=password]')).sendKeys(password);
+  }
+
+  submitForm() {
+    return element(by.css('button[type=submit]')).click();
+  }
+
+  getErrorMessage() {
+    return element(by.css('div.error')).getText();
+  }
+
 }
